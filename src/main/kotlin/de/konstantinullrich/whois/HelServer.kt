ed583fd70193abd04a46f5f136c683eb6b7d438c
Konstantin Ullrich <konstantinullrich12@gmail.com>
2020-10-20 22:48:41 +0200

    Add Example Config and Frontend Endpoint

--- src/main/kotlin/de/konstantinullrich/whois/HelServer.kt
+++ src/main/kotlin/de/konstantinullrich/whois/HelServer.kt
@@ -6,15 +6,13 @@ import de.konstantinullrich.whois.utils.WhoIs
 import org.eclipse.jetty.server.Handler
 import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.server.handler.ContextHandlerCollection
-import org.eclipse.jetty.servlet.FilterHolder
 import org.eclipse.jetty.servlet.ServletContextHandler
-import org.eclipse.jetty.servlet.ServletHandler
 import org.eclipse.jetty.servlet.ServletHolder
 
 
-//import org.eclipse.jetty.webapp.WebAppContext
+import org.eclipse.jetty.webapp.WebAppContext
 
-class HelServer(config: HelConfig) {
+class HelServer(private val config: HelConfig) {
     private val server: Server = Server(config.port)
     private val whoIs: WhoIs = WhoIs(config.whoIsServer)
 
@@ -22,21 +20,21 @@ class HelServer(config: HelConfig) {
         val handlers = arrayListOf<Handler>()
         val contexts = ContextHandlerCollection()
 
-        // handlers.add(getWebApp())
+        handlers.add(getWebApp())
         handlers.add(getRestHandler())
 
         contexts.handlers = handlers.toTypedArray()
         server.handler = contexts
     }
 
-//    private fun getWebApp(): WebAppContext {
-//        val webapp = WebAppContext()
-//        webapp.contextPath = "/*"
-//
-//        webapp.war = config.webAppPath
-//
-//        return webapp
-//    }
+    private fun getWebApp(): WebAppContext {
+        val webapp = WebAppContext()
+        webapp.contextPath = "/"
+
+        webapp.war = config.webAppPath
+
+        return webapp
+    }
 
     private fun getRestHandler(): ServletContextHandler {
         val restContext = ServletContextHandler()
